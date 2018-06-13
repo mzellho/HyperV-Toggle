66d4181d0b2da61e8b3fbdad26fdce84f40cca51
Maximilian Zellhofer <maximilian.zellhofer@prime-force.com>
2018-06-13 10:00:23 +0200

    minor formatting

--- hyperv.bat
+++ hyperv.bat
@@ -2,24 +2,22 @@
 
 FOR /F "tokens=2" %%a IN ('BCDEDIT /enum ^| FIND /i "hypervisorlaunchtype"') DO SET $CURRENT_LAUNCHTYPE=%%a
 ECHO Welcome! Hypervisor Launch Type is set to [%$CURRENT_LAUNCHTYPE%] at the moment.
-ECHO.
 
+ECHO.
 CHOICE /C DAVO /M "Do you want to set up Hypervisor Launch Type for [D]ocker/[A]uto or [V]MWare/[O]ff?" /N
-ECHO.
-
 IF "%ERRORLEVEL%" == "1" SET LAUNCHTYPE=Auto
 IF "%ERRORLEVEL%" == "2" SET LAUNCHTYPE=Auto
 IF "%ERRORLEVEL%" == "3" SET LAUNCHTYPE=Off
 IF "%ERRORLEVEL%" == "4" SET LAUNCHTYPE=Off
 IF "%$CURRENT_LAUNCHTYPE%" == "%LAUNCHTYPE%" GOTO unchanged
 
+ECHO.
 ECHO Setting Hypervisor Launch Type to [%LAUNCHTYPE%]
 BCDEDIT /set hypervisorlaunchtype %LAUNCHTYPE%
-ECHO.
 
+ECHO.
 ECHO Note: Changed Hypervisor Launch Type only take effect after a restart!
 CHOICE /C YN /M "Do you want to reboot computer in order to activate the new Hypervisor Launch Type setting [Y/N]?" /N
-ECHO.
 IF "%ERRORLEVEL%" == "1" GOTO reboot
 IF "%ERRORLEVEL%" == "2" GOTO end
 
@@ -29,10 +27,10 @@ GOTO end
 
 :unchanged
 ECHO Hypervisor Launch Type will remain unchanged [%LAUNCHTYPE%], no need to reboot.
-ECHO.
 GOTO end
 
 :end
+ECHO.
 ECHO Have a nice day!
 SET LAUNCHTYPE=
 SET $CURRENT_LAUNCHTYPE=
